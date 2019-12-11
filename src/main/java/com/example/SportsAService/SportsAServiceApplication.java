5f2ba266c8d115eb4ccc1361e454cc0fab476c86
premDSA <prem.2017@outlook.com>
2019-12-11 18:36:40 +0000

    Initial Draft

---
+++ src/main/java/com/example/SportsAService/SportsAServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.SportsAService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsAServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsAServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SportsAService/SportsAServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SportsAService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsAServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
